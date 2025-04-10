32b060a1ec5bd5621e64df25772c9e7afa695ff0
mainak58 <iammainak58@gmail.com>
2025-04-10 20:50:57 +0530

    doctor admin control complete

--- src/app/admin/doctor/page.tsx
+++ src/app/admin/doctor/page.tsx
@@ -3,10 +3,29 @@
 import React, { useEffect, useState } from "react";
 import { Doctor } from "../../../..";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function DoctorList() {
     const [doctors, setDoctors] = useState<Doctor[]>([]);
     const [loading, setLoading] = useState(true);
+    const route = useRouter();
+
+    async function deleteDoctor(id: number) {
+        try {
+            const res = await fetch("/api/admin/doctor", {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ id }),
+            });
+            if (res.ok) {
+                route.push("/");
+            }
+        } catch (error) {
+            console.log(`Error: ${error}`);
+        }
+    }
 
     useEffect(() => {
         async function fetchData() {
@@ -41,6 +60,10 @@ export default function DoctorList() {
                         height={150}
                         objectFit="cover"
                     />
+                    <button onClick={() => deleteDoctor(d.doctorId)}>
+                        {" "}
+                        Delete{" "}
+                    </button>
                 </div>
             ))}
         </div>
--- src/app/api/admin/doctor/route.ts
+++ src/app/api/admin/doctor/route.ts
@@ -1,10 +1,31 @@
 import { doctorTable } from "@/db/schema";
 import { db } from "@/lib/db";
+import { eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
 
 export async function GET() {
     try {
         const result = await db.select().from(doctorTable);
+        return NextResponse.json(result);
+    } catch (error) {
+        return NextResponse.json(
+            {
+                message: `Error in doctor route ${error}`,
+            },
+            {
+                status: 400,
+            }
+        );
+    }
+}
+
+export async function DELETE(request: Request) {
+    const body = await request.json();
+    const { id } = await body;
+    try {
+        const result = await db
+            .delete(doctorTable)
+            .where(eq(doctorTable.doctorId, id));
         return NextResponse.json(result);
     } catch (error) {
         return NextResponse.json(
