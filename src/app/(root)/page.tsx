b0ddbcf052e637a75956d1c03971a5be99241dc6
mainak58 <iammainak58@gmail.com>
2025-04-19 01:29:40 +0530

    resonsive(f)

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -8,19 +8,19 @@ import FooterSection from "@/components/FooterSection";
 export default function Home() {
     return (
         <>
-            <div className="relative w-full h-[700px] md:h-screen">
+            <div className="relative w-full h-[400px] sm:h-[500px] md:h-[600px] lg:h-screen">
                 <Image src={banner1} alt="banner" fill className="object-fit" />
                 <div className="absolute inset-0 flex items-center justify-start">
-                    <div className="max-w-2xl px-8 text-black space-y-6">
-                        <h2 className="text-4xl md:text-5xl font-bold">
+                    <div className="max-w-xs sm:max-w-md md:max-w-lg lg:max-w-2xl px-4 sm:px-6 md:px-8 text-black space-y-3 sm:space-y-4 md:space-y-6">
+                        <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold">
                             Superior Care and Skilled Physicians
                         </h2>
-                        <p className="text-lg md:text-xl leading-relaxed">
+                        <p className="text-sm sm:text-base md:text-lg lg:text-xl leading-relaxed">
                             Experience superior care provided by a team of
                             highly skilled and dedicated physicians. Your
                             well-being is our top priority.
                         </p>
-                        <button className="px-6 py-3 bg-[#0474ed] text-white hover:bg-amber-700 transition rounded-md font-semibold">
+                        <button className="px-4 sm:px-5 md:px-6 py-2 sm:py-2.5 md:py-3 bg-[#0474ed] text-white hover:bg-amber-700 transition rounded-md font-semibold text-sm sm:text-base">
                             <Link href="/appointments">Make appointment</Link>
                         </button>
                     </div>
