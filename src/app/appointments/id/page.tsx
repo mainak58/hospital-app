c778ebcc8400a106b4c5be5d1b1fffcea470064b
mainak58 <iammainak58@gmail.com>
2025-05-01 01:09:24 +0530

    bugs resolved

--- src/app/appointments/id/page.tsx
+++ src/app/appointments/id/page.tsx
@@ -1,20 +1,17 @@
 import ClientForm from "@/components/ClientForm";
 import { auth, currentUser } from "@clerk/nextjs/server";
 
-
 async function page() {
     const { userId, redirectToSignIn } = await auth();
     const user = await currentUser();
     if (!userId) return redirectToSignIn();
 
-
-    
     return (
         <>
             <ClientForm
                 name={user?.fullName || ""}
                 email={user?.primaryEmailAddress?.emailAddress || ""}
-                clerkId= {userId}
+                clerkId={userId}
             />
         </>
     );
